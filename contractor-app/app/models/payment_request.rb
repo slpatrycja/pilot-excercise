35233ac808833a66114330da53b5bf7f7b816d2b
slpatrycja <sl.patrycja.of@gmail.com>
2022-11-19 12:27:46 +0100

    Create PaymentRequest model and table

---
+++ contractor-app/app/models/payment_request.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+class PaymentRequest < ApplicationRecord
+  enum :status, { pending: 0, accepted: 1, rejected: 2 }
+
+  validates_presence_of :amount_in_cents, :currency
+
+  def amount
+    amount_in_cents / 100.0
+  end
+end
